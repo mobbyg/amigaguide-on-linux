8572f284a1310f15ead6cc1b832216feb8b063a9
Rich L <mobbyg@gmail.com>
2026-09-01 09:49:59 -0400

    Add renderer model implementation unit

---
+++ src/amigaguide/renderer_model.cpp
@@ -0,0 +1,6 @@
+#include "amigaguide/renderer_model.h"
+
+namespace amigaguide {
+// Rendering is deliberately kept separate from the parser. Qt formatting will
+// be added in the viewer layer once parser compatibility is established.
+}
