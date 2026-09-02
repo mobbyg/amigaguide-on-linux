9dbc8d7b28f3ab2252cf303dbf92714d3d8b2268
Rich L <mobbyg@gmail.com>
2026-09-02 13:01:03 -0400

    Add reader navigation and search state

--- src/app/mainwindow.h
+++ src/app/mainwindow.h
@@ -2,7 +2,13 @@
 
 #include <QMainWindow>
 
+#include <QString>
+#include <vector>
+
+class QLabel;
+class QLineEdit;
 class QTextBrowser;
+class QAction;
 
 class MainWindow final : public QMainWindow
 {
@@ -13,7 +19,26 @@ public:
 private slots:
     void openFile();
     void openLink(const QUrl& url);
+    void searchText();
+    void searchNext();
+    void searchPrevious();
+    void navigateBack();
+    void navigateForward();
+    void navigateHome();
 
 private:
+    void navigateToNode(const QString& node, bool add_history = true);
+    void updateNavigationActions();
+    void updateSearchStatus();
+
     QTextBrowser* viewer_ = nullptr;
+    QLineEdit* search_box_ = nullptr;
+    QLabel* search_status_ = nullptr;
+    QAction* back_action_ = nullptr;
+    QAction* forward_action_ = nullptr;
+    QAction* home_action_ = nullptr;
+
+    std::vector<QString> navigation_history_;
+    int history_index_ = -1;
+    QString home_node_;
 };
