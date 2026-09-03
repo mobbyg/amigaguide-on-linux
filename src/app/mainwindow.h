1e01eb8f1b0c93ce9f1c175e6cd60ec65a371b74
Rich L <mobbyg@gmail.com>
2026-09-03 14:38:37 -0400

    Add per-view state for cross-document tabs and windows

--- src/app/mainwindow.h
+++ src/app/mainwindow.h
@@ -4,15 +4,18 @@
 #include <QUrl>
 
 #include <QString>
+#include <vector>
 
 #include "amigaguide/destination.h"
 #include "amigaguide/document.h"
 #include "amigaguide/navigation.h"
 
+class QAction;
+class QActionGroup;
 class QLabel;
 class QLineEdit;
+class QTabWidget;
 class QTextBrowser;
-class QAction;
 
 class MainWindow final : public QMainWindow
 {
@@ -29,25 +32,47 @@ private slots:
     void navigateBack();
     void navigateForward();
     void navigateHome();
+    void tabChanged(int index);
+    void setCrossDocumentLinksInTab();
+    void setCrossDocumentLinksInWindow();
 
 private:
-    bool loadDocumentFile(const QString& path, const QString& requested_node, bool add_history);
-    void renderDocument();
-    amigaguide::Destination currentNodeDestination(const QString& node) const;
+    enum class CrossDocumentMode {
+        NewTab,
+        NewWindow
+    };
+
+    struct ViewState {
+        QTextBrowser* viewer = nullptr;
+        amigaguide::Document document;
+        amigaguide::NavigationHistory navigation_history;
+        QString current_document_path;
+        QString home_node;
+    };
+
+    ViewState* currentView();
+    const ViewState* currentView() const;
+    ViewState* createTab(bool switch_to);
+    bool loadDocumentFile(ViewState& view, const QString& path, const QString& requested_node, bool add_history);
+    void renderDocument(ViewState& view);
+    amigaguide::Destination currentNodeDestination(const ViewState& view, const QString& node) const;
     void navigateToNode(const QString& node, bool add_history = true);
     void navigateToDestination(const amigaguide::Destination& destination, bool add_history = true);
     void updateNavigationActions();
     void updateSearchStatus();
+    void updateTabTitle(int index);
+    void setCrossDocumentMode(CrossDocumentMode mode);
+    CrossDocumentMode crossDocumentMode() const;
 
-    QTextBrowser* viewer_ = nullptr;
+    QTabWidget* tabs_ = nullptr;
     QLineEdit* search_box_ = nullptr;
     QLabel* search_status_ = nullptr;
     QAction* back_action_ = nullptr;
     QAction* forward_action_ = nullptr;
     QAction* home_action_ = nullptr;
+    QAction* new_tab_action_ = nullptr;
+    QAction* new_window_action_ = nullptr;
+    QActionGroup* cross_document_group_ = nullptr;
 
-    amigaguide::Document document_;
-    amigaguide::NavigationHistory navigation_history_;
-    QString current_document_path_;
-    QString home_node_;
+    std::vector<ViewState> views_;
 };
