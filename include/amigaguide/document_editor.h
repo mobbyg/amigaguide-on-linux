addb170f75b8d8a5c3d388d7985e8c883f12b3db
Rich L <mobbyg@gmail.com>
2026-09-03 14:47:33 -0400

    Editor v0.5: add document property editing API

--- include/amigaguide/document_editor.h
+++ include/amigaguide/document_editor.h
@@ -8,38 +8,28 @@
 
 namespace amigaguide {
 
-enum class NodeProperty {
-    Keywords,
-    Prev,
-    Next,
-    Help,
-    Toc,
-    Index,
-    Font,
-    TabWidth
+enum class DocumentProperty {
+    Name, Author, Version, Copyright, Font, Help, Toc, Index, WordDelimiter, Width, Height, TabWidth
 };
 
-enum class NodeFlag {
-    WordWrap,
-    SmartWrap,
-    Proportional
-};
+enum class DocumentFlag { WordWrap, SmartWrap };
+enum class NodeProperty { Keywords, Prev, Next, Help, Toc, Index, Font, TabWidth };
+enum class NodeFlag { WordWrap, SmartWrap, Proportional };
 
 class DocumentEditor {
 public:
     explicit DocumentEditor(Document& document) : document_(document) {}
-
+    bool set_document_property(DocumentProperty property, std::string value, std::string* error = nullptr);
+    bool set_document_flag(DocumentFlag flag, bool enabled, std::string* error = nullptr);
     bool add_node(std::string name, std::string title, std::string* error = nullptr);
     bool rename_node(std::size_t index, std::string name, std::string* error = nullptr);
     bool set_node_title(std::size_t index, std::string title, std::string* error = nullptr);
     bool set_node_property(std::size_t index, NodeProperty property, std::string value, std::string* error = nullptr);
     bool set_node_flag(std::size_t index, NodeFlag flag, bool enabled, std::string* error = nullptr);
     bool remove_node(std::size_t index, std::string* error = nullptr);
-
 private:
     bool replace_source(std::size_t begin, std::size_t end, std::string_view replacement, std::string* error);
     static void set_error(std::string* error, std::string message);
-
     Document& document_;
 };
 
