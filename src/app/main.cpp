ebe76ed8db1b0cae52f64568e79dd0e88e2e20a7
Rich L <mobbyg@gmail.com>
2026-09-03 14:39:53 -0400

    Set application identity for persistent reader settings

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -5,6 +5,9 @@
 int main(int argc, char* argv[])
 {
     QApplication app(argc, argv);
+    app.setOrganizationName(QStringLiteral("AmigaGuide on Linux"));
+    app.setApplicationName(QStringLiteral("AmigaGuide on Linux"));
+
     MainWindow window;
     window.show();
     return app.exec();
