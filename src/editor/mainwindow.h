71114a114c1477dc1e3a1fd0f5f5f6eb25f7273f
Rich L <mobbyg@gmail.com>
2026-09-08 10:34:54 -0400

    Separate editor UI ownership from MainWindow

--- src/editor/mainwindow.h
+++ src/editor/mainwindow.h
@@ -1,17 +1,16 @@
 #pragma once
 
 #include <QMainWindow>
+#include <memory>
 
-class QCheckBox;
-class QLineEdit;
-class QListWidget;
-class QPlainTextEdit;
-class QSpinBox;
+class EditorUi;
 
 class MainWindow final : public QMainWindow {
     Q_OBJECT
+    friend class EditorUi;
 public:
     explicit MainWindow(QWidget* parent = nullptr);
+    ~MainWindow() override;
     void openFile(const QString& path);
 private slots:
     void newFile(); void open(); void save(); void saveAs(); void updateNodes(); void nodeActivated(int row);
@@ -21,9 +20,8 @@ private:
     bool maybeSave(); bool saveTo(const QString& path); void setDocumentPath(const QString& path); void loadSource(const QString& source);
     bool applyNodeEdit(const QString& operation, int row); void setNodeSelection(int row); void refreshProperties(int row);
     void replaceSource(const std::string& source, int row, const QString& message);
-    QPlainTextEdit* editor_ = nullptr; QListWidget* nodes_ = nullptr; QLineEdit* titleEdit_ = nullptr;
-    QLineEdit* keywordsEdit_ = nullptr; QLineEdit* prevEdit_ = nullptr; QLineEdit* nextEdit_ = nullptr; QLineEdit* helpEdit_ = nullptr;
-    QLineEdit* tocEdit_ = nullptr; QLineEdit* indexEdit_ = nullptr; QLineEdit* fontEdit_ = nullptr; QSpinBox* tabWidthEdit_ = nullptr;
-    QCheckBox* wordWrapEdit_ = nullptr; QCheckBox* smartWrapEdit_ = nullptr; QCheckBox* proportionalEdit_ = nullptr;
-    QString filePath_; bool updating_ = false;
+
+    std::unique_ptr<EditorUi> ui_;
+    QString filePath_;
+    bool updating_ = false;
 };
