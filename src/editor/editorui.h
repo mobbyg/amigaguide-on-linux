99dd3e374e89a224ca4b1ea282dd974b23d33af1
Rich L <mobbyg@gmail.com>
2026-09-08 10:34:46 -0400

    Add editor UI interface

---
+++ src/editor/editorui.h
@@ -0,0 +1,34 @@
+#pragma once
+
+class QCheckBox;
+class QLineEdit;
+class QListWidget;
+class QPlainTextEdit;
+class QSpinBox;
+class MainWindow;
+
+class EditorUi final {
+public:
+    explicit EditorUi(MainWindow* window);
+    ~EditorUi();
+
+    void setup();
+
+    QPlainTextEdit* editor = nullptr;
+    QListWidget* nodes = nullptr;
+    QLineEdit* titleEdit = nullptr;
+    QLineEdit* keywordsEdit = nullptr;
+    QLineEdit* prevEdit = nullptr;
+    QLineEdit* nextEdit = nullptr;
+    QLineEdit* helpEdit = nullptr;
+    QLineEdit* tocEdit = nullptr;
+    QLineEdit* indexEdit = nullptr;
+    QLineEdit* fontEdit = nullptr;
+    QSpinBox* tabWidthEdit = nullptr;
+    QCheckBox* wordWrapEdit = nullptr;
+    QCheckBox* smartWrapEdit = nullptr;
+    QCheckBox* proportionalEdit = nullptr;
+
+private:
+    MainWindow* window_ = nullptr;
+};
